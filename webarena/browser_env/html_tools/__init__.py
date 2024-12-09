023c57b29193bd973117701c7f730c9f952a0855
bochen4566 <67537324+bochen4566@users.noreply.github.com>
2024-12-09 18:04:20 -0500

    Delete webarena directory

--- webarena/browser_env/html_tools/__init__.py
+++
@@ -1,6 +0,0 @@
-from .identifier import IdentifierTool
-from .prompt import HtmlPrompt
-from .html_parser import HtmlParser
-
-from .utils import print_html_object
-from .configs import basic_attrs, mind2web_keep_attrs
